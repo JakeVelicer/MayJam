271674cbe4f5a7ff3a690dd709d40a1466ff9816
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2020-05-18 21:09:09 -0500

    movement and all that pushin

---
+++ TimeJam/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using EZCameraShake;
+
+//https://docs.unity3d.com/Packages/com.unity.inputsystem@0.2/api/UnityEngine.InputSystem.Interactions.html
+
+public class PlayerInput : MonoBehaviour
+{
+    //test comment
+    private PlayerNewInput playerControlInput = null;
+    //public Manager_Weapons weaponManager;
+
+    private Vector2 previous;
+    private Vector2 _down;
+
+    private int jumpTimer;
+    private bool jump;
+
+    private void Awake()
+    {
+        playerControlInput = new PlayerNewInput();
+    }
+
+    private void OnEnable()
+    {
+        playerControlInput.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControlInput.Player.Disable();
+    }
+
+
+    public Vector2 input
+    {
+
+        get
+        {
+            Vector2 i = Vector2.zero;
+            //i.x = Input.GetAxis("Horizontal");
+            // i.y = Input.GetAxis("Vertical");
+            i.x = playerControlInput.Player.Movement.ReadValue<Vector2>().x;
+            i.y = playerControlInput.Player.Movement.ReadValue<Vector2>().y;
+            i *= (i.x != 0.0f && i.y != 0.0f) ? .7071f : 1.0f;
+            return i.normalized;
+        }
+    }
+
+    public Vector2 down
+    {
+        get { return _down; }
+    }
+
+    public Vector2 raw
+    {
+        get
+        {
+            Vector2 i = Vector2.zero;
+            // i.x = Input.GetAxisRaw("Horizontal");
+            //  i.y = Input.GetAxisRaw("Vertical");
+            i.x = playerControlInput.Player.Movement.ReadValue<Vector2>().x;
+            i.y = playerControlInput.Player.Movement.ReadValue<Vector2>().y;
+            i *= (i.x != 0.0f && i.y != 0.0f) ? .7071f : 1.0f;
+            return i.normalized;
+        }
+    }
+
+    public bool run
+    {
+        get { return Keyboard.current.leftShiftKey.isPressed; }
+    }
+
+    public bool crouch // Was GetKeyDown
+    {
+        get { return Keyboard.current.leftCtrlKey.wasPressedThisFrame; }
+    }
+
+    public bool crouching // Was GetKey
+    {
+        get { return Keyboard.current.leftCtrlKey.isPressed; }
+    }
+
+
+    void Start()
+    {
+        jumpTimer = -1;
+    }
+
+    void Update()
+    {
+
+        _down = Vector2.zero;
+        if (raw.x != previous.x)
+        {
+            previous.x = raw.x;
+            if (previous.x != 0)
+                _down.x = previous.x;
+        }
+        if (raw.y != previous.y)
+        {
+            previous.y = raw.y;
+            if (previous.y != 0)
+                _down.y = previous.y;
+        }
+    }
+
+    public void FixedUpdate()
+    {
+
+        //if (!Input.GetKey(KeyCode.Space))
+        if (!Keyboard.current.spaceKey.isPressed)
+        {
+            jump = false;
+            jumpTimer++;
+        }
+        else if (jumpTimer > 0)
+        {
+            jump = true;
+        }
+
+    }
+
+    public bool Jump()
+    {
+        return jump;
+    }
+
+    public void ResetJump()
+    {
+        jumpTimer = -1;
+    }
+}
---
+++ TimeJam/Assets/Scripts/Player/PlayerNewInput.cs
@@ -0,0 +1,205 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Player/Controls/PlayerNewInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerNewInput : IInputActionCollection, IDisposable
+{
+    private InputActionAsset asset;
+    public @PlayerNewInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerNewInput"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""c63c3311-ac86-4223-bda1-873e8e092268"",
+            ""actions"": [
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""99e16d95-68a7-4590-ba93-508417ebdaf5"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Button"",
+                    ""id"": ""0dbaf4cc-c996-40f4-95a5-1c5e21e1b106"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""c0b8e93e-9c09-4c3f-837e-0d053e6ccbdb"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": ""Hold"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""1cd984af-baed-4529-8139-08d2810976ab"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""aa188ce9-c0ca-4922-ad62-cbdada753627"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""8a039aae-d4ae-4cb4-8fb1-2282cb4949be"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""dc7086c5-7f3d-4c88-859c-55c9c4c0165e"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""cba4b06f-20e3-4672-8fac-313bb9d689ef"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Movement;
+    public struct PlayerActions
+    {
+        private @PlayerNewInput m_Wrapper;
+        public PlayerActions(@PlayerNewInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnJump(InputAction.CallbackContext context);
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
