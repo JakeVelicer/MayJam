d932decb1c1b5acdd11267deb8b051342a12c459
Warren <guileswa@gmail.com>
2020-05-20 17:56:38 -0500

    beginning of ui interaction manager

---
+++ TimeJam/Assets/Scripts/ObjectiveSystem/ObjectiveUIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ObjectiveUIManager : MonoBehaviour
+{
+    public Image winPanel;
+
+    public void switchTabs()
+    {
+
+    }
+
+    public void ObjectiveCompletionIndication(string taskName)
+    {
+        
+    }
+
+    public TextMeshProUGUI FindTextFromName(string nameOfText)
+    {
+
+    }
+
+    public void DisplayWinUI()
+    {
+
+    }
+}
