073bd1dda94957af37a98584357eaabb4164d556
JakeVelicer <007velicer@gmail.com>
2020-05-20 03:50:04 -0500

    Interaction Working

---
+++ TimeJam/Assets/Scripts/Interactables/InteractableExample.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableExample : Interactable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void ActivateInteraction()
+    {
+        if (GetComponent<MeshRenderer>().enabled)
+        {
+            GetComponent<MeshRenderer>().enabled = false;            
+        }
+        else if (GetComponent<MeshRenderer>().enabled == false)
+        {
+            GetComponent<MeshRenderer>().enabled = true;
+        }
+
+    }
+
+}
---
+++ TimeJam/Assets/Scripts/Player/SelectionBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionBehavior : MonoBehaviour
+{
+    public float rayCastLength = 6;
+    private RaycastHit hit;
+    private GameObject selectedObject;
+    public PlayerInput playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput.playerControlInput.Player.Interact.performed += ctx => InteractButton();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var handRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(handRay, out hit, rayCastLength))
+        {
+            if (hit.transform.gameObject.GetComponent<Interactable>())
+            {
+                selectedObject = hit.transform.gameObject;
+            }
+            else
+            {
+                selectedObject = null;
+            }
+            Debug.DrawLine(handRay.origin, hit.point);
+        }
+        else
+        {
+            selectedObject = null;
+        }
+    }
+
+    public void InteractButton()
+    {
+        if (selectedObject != null)
+        {
+            selectedObject.GetComponent<Interactable>().ActivateInteraction();
+        }
+    }
+}
